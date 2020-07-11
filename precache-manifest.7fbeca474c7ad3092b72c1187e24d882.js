7d87ac01a86970846384321b662b13984de0949f
adhimaufa <adhim.aufa@gmail.com>
2020-07-11 16:59:15 +0700

    Updates

---
+++ precache-manifest.7fbeca474c7ad3092b72c1187e24d882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d7f5ba29d52ee08a390059b639e90d",
+    "url": "/react-project-sanbercode/index.html"
+  },
+  {
+    "revision": "3a5972d29ef239f3adaa",
+    "url": "/react-project-sanbercode/static/css/main.4426910d.chunk.css"
+  },
+  {
+    "revision": "7897fb87e950abe1e650",
+    "url": "/react-project-sanbercode/static/js/2.21f83874.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project-sanbercode/static/js/2.21f83874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5972d29ef239f3adaa",
+    "url": "/react-project-sanbercode/static/js/main.ddd13426.chunk.js"
+  },
+  {
+    "revision": "ed58b4a1c2fbf647fede",
+    "url": "/react-project-sanbercode/static/js/runtime-main.afcb7d81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project-sanbercode/static/media/logo.5d5d9eef.svg"
+  }
+]);
